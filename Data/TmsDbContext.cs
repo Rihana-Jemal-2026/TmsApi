24512182b201833f11ed7734c4de7bb3c057903f
Rihana <rihanawork38@gmail.com>
2026-06-25 14:03:28 -0700

    feat: exercise 4 entity configurations with IEntityTypeConfiguration

--- Data/TmsDbContext.cs
+++ Data/TmsDbContext.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using TmsApi.Entities;
+using TmsApi.Configurations;
 
 namespace TmsApi.Data;
 
 public class TmsDbContext(DbContextOptions<TmsDbContext> options)
     : DbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(TmsDbContext).Assembly);
+}
+
     public DbSet<Student> Students => Set<Student>();
 
     public DbSet<Course> Courses => Set<Course>();
